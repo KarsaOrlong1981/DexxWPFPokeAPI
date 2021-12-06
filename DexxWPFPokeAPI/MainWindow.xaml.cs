906f5c0f17405f7b4d4a4e5503908cbceb0514e2
j1981 <j1981@WINDOWS11>
2021-12-06 05:28:04 +0100

    Projektdateien hinzufügen.

---
+++ DexxWPFPokeAPI/MainWindow.xaml.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PokeApiNet;
+
+namespace DexxWPFPokeAPI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pokemon> allpokes;
+        List<Pokemon> types;
+        List<string> pokemonNames;
+        PokeApiClient pokeClient;
+        PokemonCard pokemonCard;
+        ProgressBar progressBar;
+        Grid showCards;
+        ListView listView;
+        int numberOfPoke;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri(@"C:\Users\j1981\source\repos\DexxWPFPokeAPI\DexxWPFPokeAPI\pics\273289.jpg", UriKind.Absolute));
+            this.Background = myBrush;
+            pokeClient = new PokeApiClient();
+            allpokes = new List<Pokemon>();
+            types = new List<Pokemon>();
+            progressBar = new ProgressBar();
+            showCards = new Grid();
+            pokemonNames = new List<string>();
+            listView = new ListView();
+            if (File.Exists("pokemonNames.txt"))
+            {
+                using (StreamReader sr = new StreamReader("pokemonNames.txt"))
+                {
+                    while (!(sr.EndOfStream))
+                    {
+                        pokemonNames.Add(sr.ReadLine());
+                    }
+                }
+            }
+            else
+
+               PokemonNames();
+
+        }
+
+       
+        //Gewünschte anzahl der Pokemon herunterladen
+        async void GetallPokemon(int number)
+        {
+
+            int id = 1;
+            numberOfPoke = number;
+            while (id <= number)
+            {
+                try
+                {
+                    Pokemon poke = await pokeClient.GetResourceAsync<Pokemon>(id);
+                    allpokes.Add(poke);
+                    
+                    id++;
+                }
+                catch
+                {
+                    MessageBox.Show("Lost connection to Site");
+                }
+            }
+            CreatePokemonCards(allpokes);
+
+        }
+
+        async void PokemonNames()
+        {
+            int id = 1;
+            bool findPokemon = true;
+            while (findPokemon)
+            {
+                try
+                {
+                    Pokemon poke = await pokeClient.GetResourceAsync<Pokemon>(id);
+                    pokemonNames.Add(poke.Name);
+
+                    id++;
+                }
+                catch
+                {
+                   
+                    findPokemon = false;
+                  
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("pokemonNames.txt", false, Encoding.ASCII))
+            {
+                foreach (var item in pokemonNames)
+                {
+                    sw.WriteLine(item);
+                }
+            };
+        }
+
+        private void ErrorMessage()
+        {
+            txt_Box.Text = "";
+            txt_Box.Focus();
+            MessageBox.Show("Bitte nur Zahlen von 1 - 898 eingeben", "Falsche Eingabe!!!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Txt_Box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                bool inputOK = true;
+                int input = 0;
+                try
+                {
+                    input = Convert.ToInt32(txt_Box.Text);
+                    if (input < 1 || input > 898)
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch
+                {
+                    ErrorMessage();
+                    inputOK = false;
+                }
+                if (inputOK)
+                {
+                    this.frame.Visibility = Visibility.Hidden;
+                    GetallPokemon(input);
+                    progressBar = new ProgressBar
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Width = 400,
+                        Height = 50
+                    };
+                   
+                    progressBar.Maximum = numberOfPoke - 25;
+                    progressBar.IsIndeterminate = false;
+                    double animation = Convert.ToDouble(numberOfPoke);
+                   
+                 
+
+                    Duration duration = new Duration(TimeSpan.FromSeconds(10 * (numberOfPoke / 100)));
+
+                    DoubleAnimation doubleAnimation = new DoubleAnimation(0.0, animation, duration);
+                    progressBar.BeginAnimation(ProgressBar.ValueProperty, doubleAnimation);
+                    this.grid.Children.Add(progressBar);
+                    showCards.Visibility = Visibility.Collapsed;
+                }
+              
+            }
+
+        }
+        private void CreatePokemonCards(List<Pokemon> pokemonList)
+        {
+
+            this.frame.Visibility = Visibility.Hidden;
+            int moduloCount = 1;
+            int counter = 0;
+            int row = 0;
+
+            Grid grid = new Grid { };
+            //Die erste Reihe muss gesetzt sein
+            RowDefinition rowDefinition1 = new RowDefinition();
+            rowDefinition1.Height = new GridLength(400);
+            grid.RowDefinitions.Add(rowDefinition1);
+            //zwei Spalten fest setzen
+            ColumnDefinition columnDefinition1 = new ColumnDefinition();
+            columnDefinition1.Width = new GridLength(1, GridUnitType.Star);
+            ColumnDefinition columnDefinition2 = new ColumnDefinition();
+            columnDefinition2.Width = new GridLength(1, GridUnitType.Star);
+            grid.ColumnDefinitions.Add(columnDefinition1);
+            grid.ColumnDefinitions.Add(columnDefinition2);
+            foreach (var item in pokemonList)
+            {
+
+                int column = 0;
+                pokemonCard = new PokemonCard(item, false, this.grid);
+                Frame frame = pokemonCard.CreateCard();
+                frame.HorizontalAlignment = HorizontalAlignment.Stretch;
+                frame.VerticalAlignment = VerticalAlignment.Stretch;
+                frame.Padding = new Thickness(10);
+                frame.BorderBrush = new SolidColorBrush(Colors.White);
+                frame.BorderThickness = new Thickness(5);
+                grid.Children.Add(frame);
+                Grid.SetRow(frame, row);
+                if (!(counter == 0))
+                {
+                    if (moduloCount % 2 == 0)
+                    {
+                        column = 1;
+                        RowDefinition rowDefinition = new RowDefinition();
+                        rowDefinition.Height = new GridLength(400);
+                        grid.RowDefinitions.Add(rowDefinition);
+                        row++;
+                    }
+                }
+
+                Grid.SetColumn(frame, column);
+
+                //progressBar.Value = moduloCount;
+
+                moduloCount++;
+                counter++;
+
+
+            }
+            showCards = grid;
+            this.grid.Children.Add(grid);
+            Grid.SetRow(grid, 1);
+            this.grid.Children.Remove(progressBar);
+        }
+
+      
+
+        private void backMenue_Click(object sender, RoutedEventArgs e)
+        {
+            showCards.Visibility = Visibility.Hidden;
+            this.frame.Visibility = Visibility.Visible;
+        }
+
+       
+
+        private void forward_Click(object sender, RoutedEventArgs e)
+        {
+            showCards.Visibility = Visibility.Visible;
+        }
+
+        private void buttonLos_Click(object sender, RoutedEventArgs e)
+        {
+            this.frame.Visibility = Visibility.Hidden;
+            List<string> searchResult = new List<string>();
+            string input = txt_search .Text;
+            input = input.ToLower ();
+            foreach (var item in pokemonNames)
+            {
+                bool containsResult = item.Contains(input);
+                if (containsResult)
+                {
+                   searchResult.Add(item); 
+                }
+            }
+            ListView listView = new ListView()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = 200,
+                Margin = new Thickness(0,40,0,0),
+            };
+            listView.SelectionChanged += ListView_SelectionChanged;
+            foreach (var item in searchResult)
+            {
+                listView.Items.Add(item);
+            }
+            this.listView = listView;
+            this.grid.Children.Add(listView);  
+            Grid.SetRow(listView, 0);
+        }
+
+        private async void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.grid.Children.Remove(listView);
+            this.frame.Visibility = Visibility.Hidden;
+#pragma warning disable CS8600 // Das NULL-Literal oder ein möglicher NULL-Wert wird in einen Non-Nullable-Typ konvertiert.
+            string name = Convert.ToString(e.AddedItems[0]);
+#pragma warning restore CS8600 // Das NULL-Literal oder ein möglicher NULL-Wert wird in einen Non-Nullable-Typ konvertiert.
+
+            Pokemon pokemon = await pokeClient.GetResourceAsync<Pokemon>(name);
+            pokemonCard = new PokemonCard(pokemon, true, this.grid);
+            Frame singlePokeCard = pokemonCard.CreateCard();
+            singlePokeCard.HorizontalAlignment = HorizontalAlignment.Center;
+            singlePokeCard.VerticalAlignment = VerticalAlignment.Center;
+            singlePokeCard.Margin = new Thickness(70);
+            this.grid.Children.Add(singlePokeCard);
+            Grid.SetRow(singlePokeCard, 0);
+        }
+
+        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.frame.Visibility = Visibility.Hidden;
+            this.grid.Children.Remove(listView);
+            buttonLos_Click(sender, e);
+        }
+    }
+}
+
---
+++ DexxWPFPokeAPI/PokemonCard.cs
@@ -0,0 +1,320 @@
+﻿using PokeApiNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace DexxWPFPokeAPI
+{
+    public class PokemonCard
+    {
+        Frame frame;
+        Image image;
+        Grid grid;
+        Button button;
+        Label lbl_Name;
+        Label lbl_Type;
+        SolidColorBrush cardColor, textColor, typeBackColor, typeForeColor, typeBorderColor;
+        Pokemon pokemon;
+        bool singleView;
+        public PokemonCard(Pokemon pokemon,bool singleView, Grid grid)
+        {
+            this.pokemon = pokemon;
+            this.singleView = singleView;
+            this.grid = grid;
+            frame = new Frame();
+            image = new Image();
+            button = new Button();
+            lbl_Name = new Label();
+            lbl_Type = new Label();
+            cardColor = new SolidColorBrush();
+            textColor = new SolidColorBrush();  
+            typeBackColor = new SolidColorBrush();  
+            typeForeColor = new SolidColorBrush();
+            typeBorderColor = new SolidColorBrush();
+            
+
+        }
+      
+        public Frame CreateCard()
+        {
+            List<string> typeList = new List<string>();
+            foreach (var item in pokemon.Types)
+            {
+                typeList.Add(item.Type.Name);
+            }
+            string mainType = typeList[0];
+            switch (mainType)
+            {
+                case "normal":
+                    cardColor = Brushes.Gray;
+                    typeBackColor = Brushes.LightGray;
+                    typeForeColor = Brushes.Black;
+                    typeBorderColor = Brushes.DarkGray;
+                    textColor = Brushes.White;
+                    break;
+                case "fire":
+                    cardColor = Brushes.Orange;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.DarkOrange;
+                    typeForeColor = Brushes.Red;
+                    typeBorderColor = Brushes.OrangeRed;
+                    break;
+                case "water":
+                    cardColor = Brushes.SteelBlue;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.LightBlue;
+                    typeForeColor = Brushes.DarkBlue;
+                    typeBorderColor = Brushes.Blue;
+                    break;
+                case "grass":
+                    cardColor = Brushes.SpringGreen;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.LightGreen;
+                    typeForeColor = Brushes.DarkGreen;
+                    typeBorderColor = Brushes.Green;
+                    break;
+                case "electric":
+                    cardColor = Brushes.Yellow;
+                    textColor = Brushes.Black;
+                    typeBackColor = Brushes.DarkGoldenrod;
+                    typeForeColor = Brushes.GreenYellow ;
+                    typeBorderColor = Brushes.LightYellow;
+                    break;
+                case "ice":
+                    cardColor = Brushes.DarkCyan;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.LightCyan;
+                    typeForeColor = Brushes.CadetBlue;
+                    typeBorderColor = Brushes.Cyan;
+                    break;
+                case "fighting":
+                    cardColor = Brushes.Red;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.IndianRed;
+                    typeForeColor = Brushes.White;
+                    typeBorderColor = Brushes.OrangeRed;
+                    break;
+                case "poison":
+                    cardColor = Brushes.Violet;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.LimeGreen;
+                    typeForeColor = Brushes.Violet;
+                    typeBorderColor = Brushes.PaleGreen;
+                    break;
+                case "ground":
+                    cardColor = Brushes.SandyBrown;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.LightSalmon;
+                    typeForeColor = Brushes.Brown;
+                    typeBorderColor = Brushes.RosyBrown;
+                    break;
+                case "flying":
+                    cardColor = Brushes.LavenderBlush;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.Lavender;
+                    typeForeColor = Brushes.Blue;
+                    typeBorderColor = Brushes.PaleVioletRed;
+                    break;
+                case "psychic":
+                    cardColor = Brushes.Magenta;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.DarkMagenta;
+                    typeForeColor = Brushes.LawnGreen;
+                    typeBorderColor = Brushes.LightPink;
+                    break;
+                case "bug":
+                    cardColor = Brushes.Olive;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.ForestGreen;
+                    typeForeColor = Brushes.White;
+                    typeBorderColor = Brushes.LightGreen;
+                    break;
+                case "rock":
+                    cardColor = Brushes.Goldenrod;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.Gold;
+                    typeForeColor = Brushes.DarkGray;
+                    typeBorderColor = Brushes.DarkGoldenrod;
+                    break;
+                case "ghost":
+                    cardColor = Brushes.Lavender;
+                    textColor = Brushes.Black;
+                    typeBackColor = Brushes.LavenderBlush;
+                    typeForeColor = Brushes.DarkBlue;
+                    typeBorderColor = Brushes.Blue;
+                    break;
+                case "dark":
+                    cardColor = Brushes.Black;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.DarkGray;
+                    typeForeColor = Brushes.Black;
+                    typeBorderColor = Brushes.LightGray;
+                    break;
+                case "dragon":
+                    cardColor = Brushes.Purple;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.MediumPurple;
+                    typeForeColor = Brushes.PeachPuff;
+                    typeBorderColor = Brushes.BlueViolet;
+                    break;
+                case "steel":
+                    cardColor = Brushes.LightSteelBlue;
+                    textColor = Brushes.White;
+                    typeBackColor = Brushes.SteelBlue;
+                    typeForeColor = Brushes.White;
+                    typeBorderColor = Brushes.Blue;
+                    break;
+                case "fairy":
+                    cardColor = Brushes.MistyRose;
+                    textColor = Brushes.Black;
+                    typeBackColor = Brushes.Pink;
+                    typeForeColor = Brushes.White;
+                    typeBorderColor = Brushes.Magenta;
+                    break;
+
+            }
+            // dieses Frame bildet den rahmen der karte und nimmt alle daten auf
+            frame = new Frame
+            {
+                Background = cardColor,
+               
+            };
+            if (singleView)
+            {
+                frame.Width = 450;
+                frame.Height = 300;
+             
+            }
+            Grid grid = new Grid
+            {
+               HorizontalAlignment = HorizontalAlignment.Stretch,
+               VerticalAlignment = VerticalAlignment.Stretch,
+               Background = cardColor
+            };
+
+            //Zwei Spalten, Links Image , rechts die Stats
+            ColumnDefinition columnDefinition1 = new ColumnDefinition();
+            columnDefinition1.Width = new GridLength(1, GridUnitType.Star);
+            ColumnDefinition columnDefinition2 = new ColumnDefinition();
+            columnDefinition2.Width = new GridLength(1, GridUnitType.Star);
+            grid.ColumnDefinitions.Add(columnDefinition1);
+            grid.ColumnDefinitions.Add(columnDefinition2);
+
+            // dieses Grid nimmt die rechte seite ein und unterteilt diese in die nötige Anzahl an reihen für stats und einen Button
+            Grid gridStats = new Grid
+            {
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Background = cardColor
+            };
+           
+            image = new Image
+            {
+               
+                Source = new BitmapImage(new Uri(pokemon.Sprites.FrontShiny, UriKind.Absolute)),
+            };
+            grid.Children.Add(image);
+            Grid.SetColumn(image, 0);
+
+            // Das label für die angabe des typs
+            lbl_Type = new Label
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                FontSize = 20.0,
+                Foreground = typeForeColor,
+                Background = typeBackColor,
+                Content = mainType.ToUpper(),
+                BorderBrush = typeBorderColor,
+                BorderThickness = new Thickness(5)
+            };
+            grid.Children.Add(lbl_Type);
+
+            // Das label für die angabe des Namen
+            lbl_Name = new Label
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
+                FontSize = 25.0,
+                Foreground = Brushes.Gold,
+                Background = Brushes.Black,
+                Content = pokemon.Name.ToUpper(),
+                BorderBrush = Brushes.Gold,
+                BorderThickness = new Thickness(5)
+            };
+            grid.Children.Add(lbl_Name);
+
+            // Die Statistiken der pokemon in eine Liste schreiben
+            List<String> statsList = new List<String>();
+            foreach (var item in pokemon.Stats)
+            {
+                string statistics = item.Stat.Name.ToUpper() + ": " + Convert.ToString(item.BaseStat);
+                statsList.Add(statistics);
+            }
+
+            //Über die eben erstellte Liste die Statistiken dem grid zuweisen
+            int count = 0;
+            foreach (var item in statsList)
+            {
+                Label label = new Label
+                {
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    FontSize = 15.0,
+                    Foreground = textColor,
+                    Content = item
+                };
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = new GridLength(1, GridUnitType.Star);
+                gridStats.RowDefinitions.Add(rowDefinition);
+                gridStats.Children.Add(label);
+                Grid.SetRow(label, count);
+                Grid.SetColumn(label, 1);
+                count++;
+            }
+            grid.Children.Add(gridStats);
+            Grid.SetColumn(gridStats, 1);
+
+            // Den Button zum Schließen der karte hinzufügen
+            if (singleView)
+            {
+                button = new Button
+                {
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    FontSize = 15.0,
+                    Foreground = Brushes.Orange,
+                    Background = Brushes.Black,
+                    BorderBrush = Brushes.BurlyWood,
+                    BorderThickness = new Thickness(5),
+                    Content = "Close"
+
+                };
+                button.Click += Button_Click;
+                //Eine weitere reihe für den Button hinzufügen
+                RowDefinition rowDefinition1 = new RowDefinition();
+                rowDefinition1.Height = new GridLength(1, GridUnitType.Star);
+                gridStats.RowDefinitions.Add(rowDefinition1);
+                gridStats.Children.Add(button);
+                Grid.SetRow(button, count + 1);
+                Grid.SetColumn(button, 1);
+            }
+
+
+
+            frame.Content = grid;
+            return frame;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           this.grid.Children.Remove(frame);    
+        }
+    }
+}
